47d5c093b7dd56dd387587c004d7aa83431964f2
Sebastian Cabarcas <sebascabarcas@gmail.com>
2019-10-21 14:33:20 -0500

    CardEvent component created

---
+++ components/CardEvent/index.js
@@ -0,0 +1,44 @@
+import React from 'react'
+import styles from './styles.js'
+import {View, Image} from 'react-native'
+import {Badge, Card} from 'native-base'
+import MyText from '../MyText';
+
+const CardEvent = ({ name, time, date, groupName, price, distance, source, description}) => {
+	return(
+		<Card style={styles.container}>
+			<View style={styles.dataContainer}>
+				<Image source={require('./../../assets/images/big_check.png')} style={styles.profilePhoto}/>
+				<View style={styles.subDataContainer}>
+					<View style={styles.driverNameContainer}>
+						<MyText numberOfLines={1} fontStyle="semibold" style={styles.driverNameText}> {name} </MyText>
+						{/*<MyText fontStyle="semibold" style={styles.driverNameText}> ${price} </MyText>*/}
+					</View>
+					<View style={styles.driverPriceContainer}>
+						<View style={styles.badgeContainer}>
+							<Badge style={styles.badgeItemBlueDark}>
+								<MyText fontStyle="semibold" style={styles.textTime}>{groupName}</MyText>
+							</Badge>
+							<Badge style={styles.badgeItemGray}>
+								<MyText fontStyle="semibold" style={styles.textTime}>{date}</MyText>
+							</Badge>
+							<Badge style={styles.badgeItemBlue}>
+								<MyText fontStyle="semibold" style={styles.textTime}>{time}</MyText>
+							</Badge>
+						</View>
+					</View>
+				</View>
+			</View>
+			<View style={styles.tripData}>
+				<MyText fontStyle="bold" style={styles.labelTrip}>LUGAR DEL EVENTO</MyText>
+				<MyText style={styles.labelAddress}>{source}</MyText>
+			</View>
+			<View style={styles.tripData}>
+				<MyText fontStyle="bold" style={styles.labelTrip}>DESCRIPCIÓN</MyText>
+				<MyText style={styles.labelAddress}>{description}</MyText>
+			</View>
+		</Card>
+	)
+}
+
+export default CardEvent
---
+++ components/CardEvent/styles.js
@@ -0,0 +1,106 @@
+import { StyleSheet } from 'react-native'
+import theme from './../../styles/theme.style.js'
+import {
+  widthPercentageToDP as wp,
+  heightPercentageToDP as hp,
+} from 'react-native-responsive-screen';
+
+export default StyleSheet.create({
+	container: {
+		width: '100%',
+		backgroundColor: theme.COMPLEMENTARY_COLOR,
+	},
+	dataContainer:{
+		display: 'flex',
+		width: '100%',
+		backgroundColor: theme.BACKGROUND_GRAY_COLOR,
+		justifyContent: 'space-around',
+		alignItems: 'center',
+		padding: wp(3),
+		flexDirection: 'row',
+		borderBottomWidth: 1,
+		borderColor: '#EFEFF4'
+	},
+	subDataContainer:{
+		flexDirection: 'column',
+		flex: 1,
+		marginLeft: 10,
+	},
+	driverPriceContainer:{
+		justifyContent: 'flex-start',
+		alignItems: 'flex-start',
+		flexDirection: 'row'
+	},
+	profilePhoto:{
+		width: 50,
+		height: 50,
+		borderRadius: 50/3
+	},
+	driverNameContainer:{
+		flexDirection: 'row',
+		marginBottom: 5,
+		justifyContent: 'space-between',
+	},
+	driverNameText:{
+		color: theme.SECONDARY_COLOR,
+		fontSize: theme.FONT_SIZE_LARGE
+	},
+	driverTypeText:{
+		fontSize: theme.FONT_SIZE_MEDIUM,
+		color: theme.GRAY_COLOR
+	},
+	badgeContainer:{
+		flexDirection: 'row',
+		justifyContent: 'center',
+		alignItems: 'center'
+	},
+	badgeItemBlueDark:{
+		padding: 0,
+		height: 20,
+		backgroundColor: theme.SECONDARY_COLOR,
+		justifyContent: 'center',
+		alignItems: 'center'
+	},
+	badgeItemBlue:{
+		padding: 0,
+		height: 20,
+		justifyContent: 'center',
+		alignItems: 'center',
+		backgroundColor: theme.PRIMARY_COLOR,
+		marginLeft: 5,
+	},
+	badgeItemGray:{
+		padding: 0,
+		height: 20,
+		justifyContent: 'center',
+		alignItems: 'center',
+		backgroundColor: theme.GRAY_COLOR,
+		marginLeft: 5,
+	},
+	textTime:{
+		fontSize: theme.FONT_SIZE_SMALL,
+		margin: 0,
+		padding: 0,
+		color: theme.COMPLEMENTARY_COLOR
+	},
+	tripData:{
+		paddingBottom: 15,
+		paddingTop: 10,
+		width: '100%',
+		flexDirection: 'column',
+		justifyContent: 'center',
+		alignItems: 'flex-start',
+		borderBottomWidth: 1,
+		borderColor: '#EFEFF4'
+	},
+	labelTrip: {
+		marginLeft: wp(3),
+		color: theme.GRAY_COLOR,
+		fontSize: theme.FONT_SIZE_SMALL
+	},
+	labelAddress: {
+		marginHorizontal: wp(3),
+		marginTop: 2,
+		fontSize: theme.FONT_SIZE_INPUT
+	},
+})
