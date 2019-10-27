2a8f36376dc00b2ee0c92de0a130152e583f8986
Sebastian Cabarcas <sebascabarcas@gmail.com>
2019-10-27 12:48:57 -0500

    no-profile-image added

--- constants/Images.js
+++ constants/Images.js
@@ -3,10 +3,11 @@ export default images = {
     "72261184": require('../assets/images/alejandro_montes.jpeg'),
     "123456789": require('../assets/images/jefe_invima.png'),
     "default": require('../assets/images/robot-dev.png'),
+    "no-profile-picture": require('../assets/images/no-profile-image.jpg'),
     "dashboard_bg_image": require ('../assets/images/dashboard_image.png'),
     "auth_bg_image": require ('../assets/images/Image_Background.png'),
-    "big_check": require('../assets/big_check.png'),
+    "big_check": require('../assets/images/big_check.png'),
     "password_restored": require('../assets/images/passwordrestored.png'),
-    "logo": require('../assets/images/passwordrestored.png'),
-    "raul": require('../assets/images/raul.png')
+    "logo": require('../assets/images/Logo.png'),
+    "raul": require('../assets/images/robot-dev.png')
 };
