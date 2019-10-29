b12babb3a729eb738ff6151b5e338c307c4ab2be
Sebastian Cabarcas <sebascabarcas@gmail.com>
2019-10-29 12:25:31 -0500

    no-profile-photo added to images

--- components/EditProfile/styles.js
+++
@@ -1,9 +0,0 @@
-import {StyleSheet} from 'react-native';
-import theme from '../../styles/theme.style';
-import {
-  widthPercentageToDP as wp,
-  heightPercentageToDP as hp,
-} from 'react-native-responsive-screen';
-
-export default StyleSheet.create ({
-});
