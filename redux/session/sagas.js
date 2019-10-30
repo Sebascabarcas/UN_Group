3d05864898e762d3485ab45c8c8a9c447ee137df
Sebastian Cabarcas <sebascabarcas@gmail.com>
2019-10-29 23:14:25 -0500

    Error in CHANGE_CURRENT_GROUP with yield reserved word fixed

--- redux/session/sagas.js
+++ redux/session/sagas.js
@@ -1,31 +1,34 @@
-import { all, takeEvery, takeLatest, put, call } from 'redux-saga/effects'
-import {ToastAndroid} from 'react-native'
-import { login, currentAccount, logout, register, updateUser, currentSession } from '../../services/Session';
+import {all, takeEvery, takeLatest, put, call} from 'redux-saga/effects';
+import {ToastAndroid} from 'react-native';
+import {
+  login,
+  currentAccount,
+  logout,
+  register,
+  updateUser,
+  currentSession,
+} from '../../services/Session';
 import jwt_decode from 'jwt-decode';
 import Storage from '../../services/Storage';
-import actions from './actions'
+import actions from './actions';
 import fromJsonToFormData from '../../services/helpers';
 // import { errorMessage } from '../../services/helpers'
 
-export function* LOGIN({ payload }) {
-  const { auth, storeSession, navigate, skipLoading } = payload
-  yield put({
+export function* LOGIN({payload}) {
+  const {auth, storeSession, navigate, skipLoading} = payload;
+  yield put ({
     type: 'session/SET_STATE',
     payload: {
       loading: true,
     },
-  })
+  });
   try {
-    const {token} = yield call(
-      login,
-      auth,
-      { skipLoading },
-    )
-    console.log(token)
+    const {token} = yield call (login, auth, {skipLoading});
+    console.log (token);
     // const {secret, tokenable_id: user_id, tokenable_type: role} = success
-    const {user} = jwt_decode(token);
+    const {user} = jwt_decode (token);
     // console.log(jwt_result);
-    
+
     // console.log(secret,
     //   user_id,
     //   role);
@@ -39,212 +42,216 @@ export function* LOGIN({ payload }) {
     // })
     // console.log(storeSession);
     const payload = {
-      current_user: user, 
-      isSuperAdmin: user.isSuperAdmin, 
-      myGroups: user.userGroupRelations
-    }
-    user.userGroupRelations = user.userGroupRelations.map(groupRelation => groupRelation.group)
+      current_user: user,
+      isSuperAdmin: user.isSuperAdmin,
+      myGroups: user.userGroupRelations,
+    };
+    user.userGroupRelations = user.userGroupRelations.map (
+      groupRelation => groupRelation.group
+    );
     if (user.userGroupRelations.length > 0) {
-      payload.current_group = user.userGroupRelations[0]
-      user.current_group = payload.current_group
-    }  
-    yield call (Storage.set, 'Session', {
-      secret: token,
-      user
-    }, ()  => navigate('Home'))
-    yield put({
+      payload.current_group = user.userGroupRelations[0];
+      user.current_group = payload.current_group;
+    }
+    yield call (
+      Storage.set,
+      'Session',
+      {
+        secret: token,
+        user,
+      },
+      () => navigate ('Home')
+    );
+    yield put ({
       type: 'session/SET_STATE',
-      payload
-    })
+      payload,
+    });
     ToastAndroid.show ('Bienvenido a la aplicación!', ToastAndroid.SHORT);
-    console.log('guardado');
+    console.log ('guardado');
   } catch (error) {
-    console.log(error);
+    console.log (error);
     ToastAndroid.show ('Usuario o contraseña invalidos', ToastAndroid.SHORT);
   }
-  yield put({
+  yield put ({
     type: 'session/SET_STATE',
     payload: {
       loading: false,
     },
-  })
+  });
 }
 
-export function* REGISTER({ payload }) {
-  const { user, navigate } = payload
-  yield put({
+export function* REGISTER({payload}) {
+  const {user, navigate} = payload;
+  yield put ({
     type: 'session/SET_STATE',
     payload: {
       loading: true,
     },
-  })
+  });
   try {
-    yield call(register, user)
+    yield call (register, user);
     ToastAndroid.show (
       'Usuario registrado, porfavor ingrese con su identificación y contraseña',
       ToastAndroid.SHORT
     );
-    navigate('SignIn')
+    navigate ('SignIn');
   } catch (error) {
-    console.log(error);
+    console.log (error);
     ToastAndroid.show (
       'Error en creación de usuario, verifique los campos',
       ToastAndroid.SHORT
     );
   }
-  yield put({
+  yield put ({
     type: 'session/SET_STATE',
     payload: {
       loading: false,
     },
-  })
+  });
 }
-
 
 export function* UPDATE_PROFILE({payload}) {
-  yield put({
+  yield put ({
     type: 'session/SET_STATE',
     payload: {
       loading: true,
     },
-  })
+  });
   try {
-    var {user, skipLoading, navigate } = payload
-    user = fromJsonToFormData(user)
+    var {user, skipLoading, navigate} = payload;
+    user = fromJsonToFormData (user);
     // user.password = "123456" //Por motivos de pruebas
-    console.log(user);
-    
-    const success = yield call(
-      updateUser,
-      user,
-      { skipLoading },
-    )
-    console.log(success)
-    const user_edited = success
-    
-    yield put({
+    console.log (user);
+
+    const success = yield call (updateUser, user, {skipLoading});
+    console.log (success);
+    const user_edited = success;
+
+    yield put ({
       type: 'session/SET_STATE',
       payload: {
-        current_user: user_edited
+        current_user: user_edited,
       },
-    })
-    
+    });
+
     ToastAndroid.show ('Usuario actualizado!', ToastAndroid.SHORT);
-    navigate('MyProfile')
-    console.log('guardado');
+    navigate ('MyProfile');
+    console.log ('guardado');
   } catch (error) {
-    console.log(error);
+    console.log (error);
     ToastAndroid.show ('Error actualizando usuario', ToastAndroid.SHORT);
   }
-  yield put({
+  yield put ({
     type: 'session/SET_STATE',
     payload: {
       loading: false,
     },
-  })
+  });
 }
 
-export function* LOGOUT({ payload: { skipLoading, navigate } }) {
-  yield put({
+export function* LOGOUT({payload: {skipLoading, navigate}}) {
+  yield put ({
     type: 'session/SET_STATE',
     payload: {
       loading: true,
     },
-  })
+  });
   try {
-    yield put({
+    yield put ({
       type: 'session/SET_STATE',
       payload: {
         loading: true,
       },
-    })
+    });
     // yield call(logout, { skipLoading })
-    yield call (Storage.delete, 'Session', ()  => navigate('Auth'))
+    yield call (Storage.delete, 'Session', () => navigate ('Auth'));
   } catch (error) {
     ToastAndroid.show ('Error cerrando sesión', ToastAndroid.SHORT);
   }
-  yield put({
+  yield put ({
     type: 'session/SET_STATE',
     payload: {
       loading: false,
     },
-  })
+  });
 }
-
 
 export function* CHANGE_CURRENT_GROUP({payload: {group, goBack}}) {
-  yield put({
+  yield put ({
     type: 'session/SET_STATE',
     payload: {
       loading: true,
     },
-  })
-  const current_session = yield call(currentSession)
+  });
+  const current_session = yield call (currentSession);
   if (current_session) {
-    current_session.user.current_group = group
-    yield call (Storage.set, 'Session', {
-      current_session
-    }, ()  => {
-      yield put({
-        type: 'session/SET_STATE',
-        payload: {
-          current_group: group
-        }
-      })
-      goBack();
-    }
-    )
+    current_session.user.current_group = group;
+    yield put ({
+      type: 'session/SET_STATE',
+      payload: {
+        current_group: group,
+      },
+    });
+    yield call (
+      Storage.set,
+      'Session',
+      {
+        current_session,
+      },
+      () => {
+        goBack ();
+      }
+    );
   }
-  yield put({
+  yield put ({
     type: 'session/SET_STATE',
     payload: {
       loading: false,
     },
-  })
+  });
 }
 
-export function* LOAD_CURRENT_ACCOUNT() {
-  yield put({
+export function* LOAD_CURRENT_ACCOUNT () {
+  yield put ({
     type: 'session/SET_STATE',
     payload: {
       loading: true,
     },
-  })
-  const current_user = yield call(currentAccount)
+  });
+  const current_user = yield call (currentAccount);
   // console.log('El usuario', current_user);
-  
+
   if (current_user) {
-    let {isSuperAdmin, userGroupRelations: myGroups} = current_user
+    let {isSuperAdmin, userGroupRelations: myGroups} = current_user;
     // console.log('');
-    myGroups = myGroups.map(groupRelation => groupRelation.group)
+    myGroups = myGroups.map (groupRelation => groupRelation.group);
     const payload = {
       current_user,
       isSuperAdmin,
-      myGroups
-    }
-    if (myGroups.length > 0) payload.current_group = myGroups[0]
-    yield put({
+      myGroups,
+    };
+    if (myGroups.length > 0) payload.current_group = myGroups[0];
+    yield put ({
       type: 'session/SET_STATE',
-      payload
-    })
+      payload,
+    });
   }
-  yield put({
+  yield put ({
     type: 'session/SET_STATE',
     payload: {
       loading: false,
     },
-  })
+  });
 }
 
-
-export default function* rootSaga() {
-  yield all([
-    takeLatest(actions.LOGIN, LOGIN),
-    takeLatest(actions.REGISTER, REGISTER),
-    takeLatest(actions.UPDATE_PROFILE, UPDATE_PROFILE),
-    takeLatest(actions.LOGOUT, LOGOUT),
+export default function* rootSaga () {
+  yield all ([
+    takeLatest (actions.LOGIN, LOGIN),
+    takeLatest (actions.REGISTER, REGISTER),
+    takeLatest (actions.UPDATE_PROFILE, UPDATE_PROFILE),
+    takeLatest (actions.LOGOUT, LOGOUT),
     // takeEvery(actions.LOAD_CURRENT_ACCOUNT, LOAD_CURRENT_ACCOUNT),
     // takeEvery(actions.UNAUTH_USER, UNAUTH_USER),
-    LOAD_CURRENT_ACCOUNT(),
-  ])
+    LOAD_CURRENT_ACCOUNT (),
+  ]);
 }
