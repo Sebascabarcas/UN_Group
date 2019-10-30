a2637270fcc60b58364db4311da4259bace49c0b
Sebastian Cabarcas <sebascabarcas@gmail.com>
2019-10-30 13:44:13 -0500

    Members slider centered

--- screens/MyGroup/Members/styles.js
+++ screens/MyGroup/Members/styles.js
@@ -7,58 +7,38 @@ import {
 
 export default StyleSheet.create({
 	container: {
+        flex: 1,
+        // flexDirection: 'column',
 		backgroundColor: theme.GRAY_COLOR,
-		height: '100%',
-		width: '100%',
-		// justifyContent: 'space-evenly',
-		// alignItems: 'center'
+		// height: '100%',
+		// width: '100%',
+        justifyContent: 'flex-end',
+        alignContent: 'flex-end',
+		alignItems: 'flex-end',
 	},
     scrollview: {
-        flex: 1
+        // flex: 1,
     },
-	// containerButtons:{
-	// 	flexDirection: 'column',
-	// 	justifyContent: 'center',
-	// 	alignItems: 'center'
-	// },
-	// buttonItem:{
-	// 	paddingHorizontal: wp(5),
-	// 	borderRadius: theme.BORDER_RADIUS_MEDIUM,
-	// 	backgroundColor: theme.COMPLEMENTARY_COLOR
-	// },
+	scrollviewContainer:{
+        flex: 1,
+        // alignContent: 'flex-end',
+        // flexGrow: 1,
+        justifyContent : 'center',
+        // justifyContent: 'space-around',
+        // alignItems: 'baseline',
+	},
+	sliderContainer:{
+        // backgroundColor: 'red',
+        // flex: 1,
+        // alignContent: 'flex-end',
+        // justifyContent: 'space-around',
+        // alignItems: 'baseline',
+    },
+	slider:{
+	},
 	textItemButton:{
 		fontSize: theme.FONT_SIZE_MEDIUM,
         color: theme.SECONDARY_COLOR
 	},
-    
     ////
-    exampleContainer: {
-        paddingVertical: 30
-    },
-    exampleContainerDark: {
-        backgroundColor: '#1a1917'
-    },
-    exampleContainerLight: {
-        backgroundColor: 'white'
-    },
-    title: {
-        paddingHorizontal: 30,
-        backgroundColor: 'transparent',
-        color: 'rgba(255, 255, 255, 0.9)',
-        fontSize: 20,
-        fontWeight: 'bold',
-        textAlign: 'center'
-    },
-    titleDark: {
-        color: '#1a1917'
-    },
-    subtitle: {
-        marginTop: 5,
-        paddingHorizontal: 30,
-        backgroundColor: 'transparent',
-        color: 'rgba(255, 255, 255, 0.75)',
-        fontSize: 13,
-        fontStyle: 'italic',
-        textAlign: 'center'
-    },
 })
