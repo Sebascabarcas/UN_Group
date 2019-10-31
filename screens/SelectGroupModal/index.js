c81e71b64913f047cc1596b1f077372eee4fa948
Sebastian Cabarcas <sebascabarcas@gmail.com>
2019-10-31 15:51:40 -0500

    SelectGroupModal fixed

--- screens/SelectGroupModal/index.js
+++ screens/SelectGroupModal/index.js
@@ -4,7 +4,7 @@ import {Ionicons} from '@expo/vector-icons';
 // import {Divider, Button} from 'react-native-elements';
 import {Button, CheckBox} from 'native-base';
 import {View, Dimensions, Image} from 'react-native';
-import { TouchableWithoutFeedback } from 'react-native-gesture-handler';
+import {TouchableWithoutFeedback} from 'react-native-gesture-handler';
 import {useSelector, useDispatch} from 'react-redux';
 import theme from '../../styles/theme.style';
 import styles from './styles.js';
@@ -14,54 +14,65 @@ import getEnvVars from '../../environment';
 
 const GOOGLE_MAPS_APIKEY = 'AIzaSyCWPrODz1hIw-3g2gX94dTJTspvq768GOw';
 const {width, height} = Dimensions.get ('window');
-const { apiUrl } = getEnvVars();
+const {apiUrl} = getEnvVars ();
 
-export default SelectGroupModal = () => {
+export default (SelectGroupModal = () => {
   const {goBack} = useNavigation ();
-  const dispatch = useDispatch();
-  const {myGroups, current_group} = useSelector(state => state.session)
-  console.log(current_group);
-  console.log(myGroups);
-  
+  const dispatch = useDispatch ();
+  const {myGroups, current_group} = useSelector (state => state.session);
+  console.log (current_group);
+  // console.log (myGroups);
+
   // const originMarker = {lat: originLocation.coords.latitude, lng: originLocation.coords.longitude}
   // const destinyMarker = {lat: destinyLocation.coords.latitude, lng: destinyLocation.coords.longitude}
-  const [loading, _setLoading] = useState (false)
+  const [loading, _setLoading] = useState (false);
 
-  const selectGroup = (group) => {
-    dispatch({
+  const _selectGroup = group => {
+    dispatch ({
       type: 'session/CHANGE_CURRENT_GROUP',
-      payload: {current_group: group, goBack}
-    })
-  }
-    return (
-      <View style={styles.container}>
-        <View style={styles.selectGroupContainer}>
-          <View style={styles.detailsContainer}>
-            <MyText fontStyle="bold" style={styles.detailsTitle}>
-              Seleccione su grupo
-            </MyText>
-            {myGroups.map(group => 
-            (<BigListItem key={group.id}
-            leftItem={
-                <TouchableWithoutFeedback style={styles.imgView} onPress={() => selectGroup(group)}>
-                    <Image style={styles.img} resizeMode="cover" source={ group.groupPicture ? {uri: `${apiUrl}${group.groupPicture.groupPictureName}`} : images['no-circle-photo']}/>
-                </TouchableWithoutFeedback>
-                }
-                primaryText={group.groupName}
-                primaryTextStyles={{paddingTop: 5, paddingBottom: 2}}
-            rightItem={
-                    <CheckBox
-                    color={theme.PRIMARY_COLOR}
-                    // fontFamily={}
-                    checked={group.id === current_group.id}
-                    onPress={() => selectGroup(group)}
-                    />
-            }
-            />)
-            )}
-            
-          </View>
+      payload: {group, goBack},
+    });
+  };
+  return (
+    <View style={styles.container}>
+      <View style={styles.selectGroupContainer}>
+        <View style={styles.detailsContainer}>
+          <MyText fontStyle="bold" style={styles.detailsTitle}>
+            Seleccione su grupo
+          </MyText>
+          {myGroups.map (group => (
+            <BigListItem
+              key={group.id}
+              onPress={() => {_selectGroup (group);}}
+              leftItem={
+                <View style={styles.imgView}>
+                  <Image
+                    style={styles.img}
+                    resizeMode="cover"
+                    source={
+                      group.groupPicture
+                        ? {
+                            uri: `${apiUrl}${group.groupPicture.groupPictureName}`,
+                          }
+                        : images['no-circle-photo']
+                    }
+                  />
+                </View>
+              }
+              primaryText={group.groupName}
+              primaryTextStyles={{paddingTop: 5, paddingBottom: 2}}
+              rightItem={
+                <CheckBox
+                  color={theme.PRIMARY_COLOR}
+                  // fontFamily={}
+                  checked={group.id === current_group.id}
+                />
+              }
+            />
+          ))}
+
         </View>
       </View>
-    );
-}
+    </View>
+  );
+});
