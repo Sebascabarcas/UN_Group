9f445e588517f0381d55b9f2214a14d8c181cb56
Sebastian Cabarcas <sebascabarcas@gmail.com>
2019-11-06 00:09:05 -0500

    image abstractgradient4 added

--- constants/Images.js
+++ constants/Images.js
@@ -1,7 +1,4 @@
 export default images = {
-    "8787433": require('../assets/images/juan_pacheco.jpeg'),
-    "72261184": require('../assets/images/alejandro_montes.jpeg'),
-    "123456789": require('../assets/images/jefe_invima.png'),
     "default": require('../assets/images/robot-dev.png'),
     "no-profile-picture": require('../assets/images/no-profile-image.jpg'),
     "no-circle-photo": require('../assets/images/no-circle-photo.png'),
@@ -11,5 +8,6 @@ export default images = {
     "big_check": require('../assets/images/big_check.png'),
     "password_restored": require('../assets/images/passwordrestored.png'),
     "logo": require('../assets/images/Logo.png'),
-    "raul": require('../assets/images/robot-dev.png')
+    "raul": require('../assets/images/robot-dev.png'),
+    "abstractgradient4": require('../assets/images/abstractgradient4.png'),
 };
