4bf0fceb955f52a0e819a9a29ea47196b16b5623
Sebastian Cabarcas <sebascabarcas@gmail.com>
2019-11-07 08:38:00 -0500

    CardEvent moment used for dates

--- components/CardEvent/index.js
+++ components/CardEvent/index.js
@@ -1,20 +1,24 @@
 import React from 'react'
 import styles from './styles.js'
-import {View, Image} from 'react-native'
+import {View, Image, TouchableOpacity} from 'react-native'
 import {Badge, Card} from 'native-base'
+import moment from 'moment'
 import {AntDesign, Ionicons} from '@expo/vector-icons'
 import MyText from '../MyText';
-import Images from '../../constants/Images.js';
+import images from '../../constants/Images.js';
 import theme from '../../styles/theme.style.js'
+import getEnvVars from '../../environment.js'
 
-const CardEvent = ({ name, time, date, groupName, price, distance, source, description}) => {
+const {apiUrl} = getEnvVars ();
+
+const CardEvent = ({ eventName, date, group: {groupName, groupPicture}, location, description, onPress}) => {
 	return(
-		<Card style={styles.container}>
+		<TouchableOpacity style={styles.container} onPress={onPress}>
 			<View style={styles.dataContainer}>
-				<Image source={Images['big_check']} style={styles.profilePhoto}/>
+				<Image source={groupPicture ? {uri: `${apiUrl}${groupPicture.groupPictureName}`} : images['logo']} style={styles.profilePhoto}/>
 				<View style={styles.subDataContainer}>
 					<View style={styles.groupNameContainer}>
-						<MyText numberOfLines={1} fontStyle="semibold" style={styles.groupNameText}> {name} </MyText>
+						<MyText numberOfLines={1} fontStyle="semibold" style={styles.groupNameText}> {eventName} </MyText>
 						{/*<MyText fontStyle="semibold" style={styles.groupNameText}> ${price} </MyText>*/}
 					</View>
 					<View style={styles.driverPriceContainer}>
@@ -28,7 +32,7 @@ const CardEvent = ({ name, time, date, groupName, price, distance, source, descr
 									color={theme.COMPLEMENTARY_COLOR}
 									size={theme.ICON_SIZE_SUPER_EXTRA_SMALL}
 								/> */}
-								<MyText fontStyle="semibold" style={styles.textTime}>{date}</MyText>
+								<MyText fontStyle="semibold" style={styles.textTime}>{moment(date).format('YYYY-MM-DD')}</MyText>
 							</Badge>
 							<Badge style={styles.badgeItemBlue}>
 								{/* <AntDesign
@@ -36,7 +40,7 @@ const CardEvent = ({ name, time, date, groupName, price, distance, source, descr
 									color={theme.COMPLEMENTARY_COLOR}
 									size={theme.ICON_SIZE_SUPER_EXTRA_SMALL}
 								/> */}
-								<MyText fontStyle="semibold" style={styles.textTime}>{time}</MyText>
+								<MyText fontStyle="semibold" style={styles.textTime}>{moment(date).format('hh:mm A')}</MyText>
 							</Badge>
 						</View>
 					</View>
@@ -44,13 +48,13 @@ const CardEvent = ({ name, time, date, groupName, price, distance, source, descr
 			</View>
 			<View style={styles.eventData}>
 				<MyText fontStyle="bold" style={styles.labelEvent}>LUGAR DEL EVENTO</MyText>
-				<MyText style={styles.labelAddress}>{source}</MyText>
+				<MyText style={styles.labelAddress}>{location}</MyText>
 			</View>
 			<View style={styles.eventData}>
 				<MyText fontStyle="bold" style={styles.labelEvent}>DESCRIPCIÓN</MyText>
 				<MyText style={styles.labelAddress}>{description}</MyText>
 			</View>
-		</Card>
+		</TouchableOpacity>
 	)
 }
 
