12d884dad5587990cb69ecbfb4b98133590a41a9
Sebastian Cabarcas <sebascabarcas@gmail.com>
2019-11-08 11:21:09 -0500

    createEvent using Requests service

--- services/Events.js
+++ services/Events.js
@@ -8,26 +8,30 @@ export async function getEvents({ skipLoading } = {}) {
     return Requests.get(`events`).then(events => events.data)
 }
 
+export async function getUserEvents({ skipLoading } = {}) {
+    return Requests.get(`events`).then(events => events.data)
+}
+
 export async function getEvent(id, { skipLoading } = {}) {
     return Requests.get(`events/${id}`).then(event => event.data)
 }
 
 export async function createEvent(groupId, newEvent, { skipLoading } = {}) {
-    // return Requests.post(`groups`, newEvent, { skipLoading, headers: {'Content-Type': 'multipart/form-data'}}).then(group => group.data)
-    console.log('newEvent:', newEvent);
+    return Requests.post(`groups/${groupId}/addEvent`, newEvent, { skipLoading}).then(new_event => new_event.data)
     
-    let {secret} = await Storage.get('Session')
-    return fetch(`${apiUrl}/UNGroup/API/groups/${groupId}/addEvent`, {
-        method: 'POST',
-        body: newEvent,
-        headers: {
-          'content-type': 'multipart/form-data',
-          'authorization': `Bearer ${secret}`
-        },
-      }).then(res => res.json());
+    // let {secret} = await Storage.get('Session')
+    // return fetch(`${apiUrl}/UNGroup/API/groups/${groupId}/addEvent`, {
+    //     method: 'POST',
+    //     body: newEvent,
+    //     headers: {
+    //       'content-type': 'multipart/form-data',
+    //       'authorization': `Bearer ${secret}`
+    //     },
+    //   }).then(res => res.json());
 }
 
 export async function getEventAttendees(id, { skipLoading } = {}) {
     return Requests.get(`events/${id}/attendees`).then(eventAttendees => eventAttendees.data)
 }
+
 
