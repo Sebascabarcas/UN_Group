d753d46ac5cd91b9a0c788ab7d1e3b8e469e3082
Sebastian Cabarcas <sebascabarcas@gmail.com>
2019-11-08 22:36:01 -0500

    Animations contants for require lottie files done

---
+++ constants/Animations.js
@@ -0,0 +1,7 @@
+export default animations = {
+    "cloud-disconnection": require('../assets/lotties/cloud-disconnection.json'),
+    "empty-box": require('../assets/lotties/empty-box.json'),
+    "empty-gabinete": require('../assets/lotties/empty-gabinete.json'),
+    "falling-empty-box": require('../assets/lotties/falling-empty-box.json'),
+    "error-tv": require('../assets/lotties/error-tv.json')
+};
