caa6c828cd74ce4f69c39a74ee11e8be431a4d79
Sebastian Cabarcas <sebascabarcas@gmail.com>
2019-11-10 06:29:17 -0500

    FloatingUser component

--- constants/Animations.js
+++ constants/Animations.js
@@ -3,5 +3,10 @@ export default animations = {
     "empty-box": require('../assets/lotties/empty-box.json'),
     "empty-gabinete": require('../assets/lotties/empty-gabinete.json'),
     "falling-empty-box": require('../assets/lotties/falling-empty-box.json'),
-    "error-tv": require('../assets/lotties/error-tv.json')
+    "error-tv": require('../assets/lotties/error-tv.json'),
+    "user-scanning": require('../assets/lotties/user-scanning.json'),
+    "user-scanning2": require('../assets/lotties/user-scanning2.json'),
+    "search-users": require('../assets/lotties/search-users.json'),
+    "minnion-looking": require('../assets/lotties/minnion-looking.json'),
+    "searching-around": require('../assets/lotties/searching-around.json'),
 };
