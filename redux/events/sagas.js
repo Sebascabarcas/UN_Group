d364a04b78ef0a5b5566614fe5c95024aae01d3e
Sebastian Cabarcas <sebascabarcas@gmail.com>
2019-11-12 03:56:52 -0500

    New action FIND_LOCATION in events

--- redux/events/sagas.js
+++ redux/events/sagas.js
@@ -5,11 +5,51 @@ import { createEvent, getEvent, getEvents, getEventAttendees } from '../../servi
 import fromJsonToFormData from '../../services/helpers';
 import moment from 'moment';
 import { getUserEvents } from '../../services/Session';
+import { getLocation } from '../../services/Location';
 // import moment from 'moment-timezone'
 // import 'moment/locale/es'  // without this line it didn't work
 // moment.locale('es')
 // import { errorMessage } from '../../services/helpers'
 
+
+export function* FIND_LOCATION({payload: {event, navigate}}) {
+  yield put ({
+    type: 'events/SET_STATE',
+    payload: {
+      loading: true,
+    },
+  });
+  try {
+    const {results} = yield call (getLocation, `${event.typeOfRoad}%20${event.mainRoad}%20%23${event.secondaryRoad}-${event.roadNumber}%2C%20Barranquilla%2C%20Atl%C3%A1ntico`);
+    console.log(results);
+    let location_result = results[0] || {geometry: {location: {lat: 10.966981761315, lng: -74.79788233489182}}}
+    const {geometry: {location: {lat: latitude, lng: longitude}}} = location_result
+    console.log('latitude: ', latitude);
+    console.log('longitude: ', longitude);
+    yield put({
+      type: 'events/SET_STATE',
+      payload: {new_event: {...event, latitude, longitude, location: `${event.typeOfRoad} ${event.mainRoad} # ${event.secondaryRoad} - ${event.roadNumber}`}}
+    })
+    navigate('AddLocation')
+    // ToastAndroid.show (
+    //   'Usuario registrado, porfavor ingrese con su identificación y contraseña',
+    //   ToastAndroid.SHORT
+    // );
+    // navigate ('SignIn');
+  } catch (error) {
+    console.log (error);
+    ToastAndroid.show (
+      'Error encontrando dirección, verifique los campos',
+      ToastAndroid.SHORT
+    );
+  }
+  yield put ({
+    type: 'events/SET_STATE',
+    payload: {
+      loading: false,
+    },
+  });
+}
 
 export function* CREATE_EVENT({ payload: { groupId, event, navigate, skipLoading } }) {
   yield put({
@@ -27,10 +67,13 @@ export function* CREATE_EVENT({ payload: { groupId, event, navigate, skipLoading
     // event = fromJsonToFormData(event)
     // console.log(event);
     const success = yield call(createEvent, groupId, event, {skipLoading});
+    console.log(success);
+    
     ToastAndroid.show ('Evento creado correctamente!', ToastAndroid.SHORT);
     navigate('Events')
     // console.log(success);
-  } catch (error) {
+  } catch (error) {CREATE_EVENT
+    CREATE_EVENT
     console.log('CREATE_EVENT, ERROR:', error);
     // errorMessage(error.response, { title: 'Fetch de localidad fallida!' })
     ToastAndroid.show ('Error creando evento!', ToastAndroid.SHORT);
@@ -127,6 +170,7 @@ export function* GET_EVENT_ATTENDEES({ payload: { id, skipLoading } }) {
 export default function* rootSaga() {
   yield all([
     takeLatest(actions.CREATE_EVENT, CREATE_EVENT),
+    takeLatest(actions.FIND_LOCATION, FIND_LOCATION),
     takeLatest(actions.GET_EVENTS, GET_EVENTS),
     takeLatest(actions.GET_EVENT, GET_EVENT),
     takeLatest(actions.GET_EVENT_ATTENDEES, GET_EVENT_ATTENDEES),
