54f4cc20ddc75c456decdb9a83474ace5f8a14e3
Sebastian Cabarcas <sebascabarcas@gmail.com>
2019-11-12 03:57:59 -0500

    AddLocation screen created

---
+++ screens/Events/AddLocation/index.js
@@ -0,0 +1,153 @@
+import React, {useEffect, useState, useRef} from 'react';
+import {View, Dimensions, ScrollView} from 'react-native';
+import {useNavigation} from 'react-navigation-hooks';
+import MapView from 'react-native-maps';
+import {
+  Button,
+  Container,
+} from 'native-base';
+import {
+  widthPercentageToDP as wp,
+  heightPercentageToDP as hp,
+} from 'react-native-responsive-screen';
+import {useDispatch, useSelector} from 'react-redux';
+import { Ionicons, FontAwesome } from '@expo/vector-icons';
+import theme from '../../../styles/theme.style';
+import mapStyle from '../../../styles/mapStyle.json';
+import MyText from '../../../components/MyText';
+import styles from './styles';
+
+const {height: fullHeight} = Dimensions.get ('window');
+
+const AddLocation = () => {
+  const mapRef = useRef (null);
+  const {current_group: group} = useSelector (state => state.session);
+  const {new_event: event} = useSelector (state => state.events);
+  var mapRegion = {
+    latitude: event.latitude,
+    longitude: event.longitude,
+    latitudeDelta: 0.00922,
+    longitudeDelta: 0.00421
+  }
+  const [marginBottom, _setMarginBottom] = useState (1);
+  const [loading, _setLoading] = useState (false);
+  const [offSet, _setOffSet] = useState (0);
+  const dispatch = useDispatch ();
+  const {navigate, setParams} = useNavigation ();
+  
+  useEffect (() => {
+    setParams({'address': event.location})
+  }, []);
+
+  _onMapReady = () => {
+    _setMarginBottom (0);
+  };
+
+  _setLocationOnRegionChanged = async region => {
+    const {latitude, longitude} = region;
+    dispatch({
+      type: 'events/SET_STATE',
+      payload: {
+        new_event: {...event, latitude, longitude}
+      }
+    })
+  };
+
+
+  return (
+    
+    <Container style={styles.container}>
+      <FontAwesome
+        name="map-marker"
+        style={{
+          zIndex: 3,
+          position: 'absolute',
+          marginTop: -87.5,
+          marginLeft: -11.5,
+          left: '50%',
+          top: '50%',
+        }}
+        size={40}
+        color="#f00"
+      />
+      <MapView
+        customMapStyle={mapStyle}
+        rotateEnabled={false}
+        pitchEnabled={false}
+        zoomControlEnabled={true}
+        // zoomEnabled={false}
+        ref={mapRef}
+        // onRegionChangeComplete={_setLocationOnRegionChanged}
+        onRegionChangeComplete={_setLocationOnRegionChanged}
+        style={{
+          alignSelf: 'stretch',
+          flex: 1,
+          marginBottom,
+          width: wp(100),
+          height: hp(100),
+          // borderRadius: 10
+        }}
+        // region={mapRegion}
+        initialRegion={mapRegion}
+        onMapReady={_onMapReady}
+        // onRegionChange={this._handleMapRegionChange}
+        showsUserLocation
+        showsMyLocationButton
+      />
+      <Button
+        primary
+        full
+        onPress={() => {
+          dispatch({
+            type: 'events/CREATE_EVENT', 
+            payload: {groupId: group.id, event, navigate}
+          })
+          // navigate ('Groups');
+        }}
+        // onPress={() => navigate('EditProfile')}
+        style={styles.actionBottomButton}
+      >
+        <MyText
+          style={{fontSize: theme.FONT_SIZE_LARGE}}
+          fontStyle="bold"
+          color="white"
+        >
+          CONFIRMAR UBICACIÓN
+        </MyText>
+      </Button>
+    </Container>
+  );
+};
+
+AddLocation.navigationOptions = ({navigation}) => {
+  return {
+    // title: '',
+    title: navigation.getParam('address'),
+    headerTransparent: true,
+    headerStyle: {
+      backgroundColor: 'transparent',
+    },
+    headerTitleStyle: {
+      fontFamily: theme.FONT_FAMILY_SEMIBOLD,
+      fontSize: theme.FONT_SIZE_MEDIUM,
+      color: theme.DARK_COLOR
+    },
+    headerLeft: (
+      <Button
+        // block
+        style={{marginLeft: 20}}
+        iconLeft
+        transparent
+        onPress={() => navigation.goBack ()}
+      >
+        <Ionicons
+          name="ios-arrow-back"
+          color={theme.PRIMARY_COLOR}
+          size={theme.ICON_SIZE_SMALL}
+        />
+      </Button>
+    ),
+  };
+};
+
+export default AddLocation;
