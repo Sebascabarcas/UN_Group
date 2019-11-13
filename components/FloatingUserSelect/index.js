e50b086257307dcb84d8df65ec50a461e20f6c83
Sebastian Cabarcas <sebascabarcas@gmail.com>
2019-11-13 15:22:56 -0500

    FloatingUserSelect component for CreateTask screen

---
+++ components/FloatingUserSelect/index.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import styles from './styles';
+import {View, Image, TouchableOpacity} from 'react-native';
+import {Badge, Card} from 'native-base';
+import MyText from '../MyText';
+import Images from '../../constants/Images.js';
+import getEnvVars from '../../environment.js';
+import { Ionicons } from '@expo/vector-icons';
+import theme from '../../styles/theme.style';
+
+const {apiUrl} = getEnvVars();
+const FloatingUserSelect = ({firstName, selected, picture, firstLastname, onPress}) => {
+  console.log(selected);
+  
+  return (
+    <TouchableOpacity
+      activeOpacity={1}
+      style={styles.slideInnerContainer}
+      onPress={onPress}
+    >
+      {selected && <View style={[styles.selectIconContainer]}>
+        <Ionicons
+          name="ios-checkmark-circle"
+          color="green"
+          size={theme.ICON_SIZE_MEDIUM}
+        />
+      </View>}
+      <View style={[styles.imageContainer]}>
+        <Image
+          resizeMode="cover"
+          source={picture ? {uri: `${picture.uri}`} : Images['no-profile-photo']}
+          style={styles.image}
+        />
+      </View>
+      <View
+        style={[styles.textContainer]}
+      >
+        <MyText
+          fontStyle="semibold"
+          style={[styles.title]}
+          // numberOfLines={2/}
+        >
+          {firstName} {firstLastname}
+        </MyText>
+      </View>
+    </TouchableOpacity>
+  );
+};
+
+export default FloatingUserSelect;
---
+++ components/FloatingUserSelect/styles.js
@@ -0,0 +1,108 @@
+import { StyleSheet } from 'react-native'
+import theme from '../../styles/theme.style.js'
+import {
+  widthPercentageToDP as wp,
+  heightPercentageToDP as hp,
+} from 'react-native-responsive-screen';
+import { Dimensions, Platform } from 'react-native';
+const colors = {
+    black: '#1a1917',
+    gray: '#888888',
+    background1: '#B721FF',
+    background2: '#21D4FD'
+};
+
+const IS_IOS = Platform.OS === 'ios';
+const slideHeight = hp(20);
+const slideWidth = wp(25);
+const itemHorizontalMargin = wp(2);
+
+const entryBorderRadius = 8;
+
+export default StyleSheet.create({
+    slideInnerContainer: {
+        width: slideWidth,
+        height: slideHeight,
+        // backgroundColor: 'red',
+        // paddingHorizontal: itemHorizontalMargin,
+        marginBottom: 5,
+        paddingBottom: 18 // needed for shadow
+    },
+    selectIconContainer: {
+        position: 'absolute',
+        zIndex: 1,
+        right: -10
+    },
+    shadow: {
+        position: 'absolute',
+        top: 0,
+        left: itemHorizontalMargin,
+        right: itemHorizontalMargin,
+        bottom: 18,
+        shadowColor: colors.black,
+        shadowOpacity: 0.25,
+        shadowOffset: { width: 0, height: 10 },
+        shadowRadius: 10,
+        borderRadius: entryBorderRadius
+    },
+    imageContainer: {
+        // flex: 1,
+        marginBottom: IS_IOS ? 0 : -1, // Prevent a random Android rendering issue
+        backgroundColor: 'white',
+        height: hp(15),
+        width: wp(25),
+        borderRadius: wp(10),
+    },
+    imageContainerEven: {
+        backgroundColor: colors.black,
+    },
+    image: {
+        // ...StyleSheet.absoluteFillObject,
+        // resizeMode: 'cover',
+        height: hp(15),
+        width: wp(25),
+        borderRadius: wp(10)
+    },
+    // image's border radius is buggy on iOS; let's hack it!
+    radiusMask: {
+        position: 'absolute',
+        bottom: 0,
+        left: 0,
+        right: 0,
+        height: entryBorderRadius,
+        backgroundColor: 'white'
+    },
+    radiusMaskEven: {
+        backgroundColor: colors.black
+    },
+    textContainer: {
+        justifyContent: 'center',
+        paddingTop: 15 - entryBorderRadius,
+		paddingBottom: 5,
+        // paddingHorizontal: wp(5),
+        backgroundColor: 'transparent',
+        borderBottomLeftRadius: entryBorderRadius,
+        borderBottomRightRadius: entryBorderRadius
+    },
+    textContainerEven: {
+		backgroundColor: colors.black
+    },
+    title: {
+        color: 'white',
+        textAlign: 'center',
+        fontSize: theme.FONT_SIZE_SMALL,
+        // letterSpacing: 0.5
+    },
+    titleEven: {
+        color: 'white'
+    },
+    subtitle: {
+        marginTop: 6,
+        color: colors.gray,
+        fontSize: 12,
+        fontStyle: 'italic'
+    },
+    subtitleEven: {
+        color: 'rgba(255, 255, 255, 0.7)'
+    }
+});
