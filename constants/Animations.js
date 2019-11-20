b5d01cb983821cf304b33eb91aafc768326fbe2a
Sebastian Cabarcas <sebascabarcas@gmail.com>
2019-11-20 03:49:52 -0500

    New animation on Animatios (role-models)

--- constants/Animations.js
+++ constants/Animations.js
@@ -6,6 +6,8 @@ export default animations = {
     "error-tv": require('../assets/lotties/error-tv.json'),
     "user-scanning": require('../assets/lotties/user-scanning.json'),
     "user-scanning2": require('../assets/lotties/user-scanning2.json'),
+    "role-models": require('../assets/lotties/role-models.json'),
+    "role-models2": require('../assets/lotties/role-models2.json'),
     "search-users": require('../assets/lotties/search-users.json'),
     "minnion-looking": require('../assets/lotties/minnion-looking.json'),
     "searching-around": require('../assets/lotties/searching-around.json'),
