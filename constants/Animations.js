7ab3083125722fbdf1b6b9bf3595cee920c32893
Sebastian Cabarcas <sebascabarcas@gmail.com>
2019-11-20 05:15:58 -0500

    mentors animation added

--- constants/Animations.js
+++ constants/Animations.js
@@ -7,7 +7,7 @@ export default animations = {
     "user-scanning": require('../assets/lotties/user-scanning.json'),
     "user-scanning2": require('../assets/lotties/user-scanning2.json'),
     "role-models": require('../assets/lotties/role-models.json'),
-    "role-models2": require('../assets/lotties/role-models2.json'),
+    "mentors": require('../assets/lotties/mentors.json'),
     "search-users": require('../assets/lotties/search-users.json'),
     "minnion-looking": require('../assets/lotties/minnion-looking.json'),
     "searching-around": require('../assets/lotties/searching-around.json'),
