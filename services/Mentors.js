40d0278ddb320f39af6d766bba2426280700d6db
Sebastian Cabarcas <sebascabarcas@gmail.com>
2019-11-20 05:15:42 -0500

    Service Mentors created

--- services/Mentors.js
+++ services/Mentors.js
@@ -1,31 +1,50 @@
 import Requests from './Requests'
 
-export async function getRoleModels(groupId, { skipLoading } = {}) {
-    return Requests.get(`groups/${groupId}/roleModels`, {skipLoading}).then(posts => posts.data)
+export async function getMentors(groupId, { skipLoading } = {}) {
+    return Requests.get(`groups/${groupId}/mentors`, {skipLoading}).then(mentors => mentors.data)
 }
 
-export async function getRoleModelsPosts(groupId, { skipLoading } = {}) {
-    return Requests.get(`groups/${groupId}/posts`, {skipLoading}).then(posts => posts.data)
+export async function getMentorActivities(userId, { skipLoading } = {}) {
+    return Requests.get(`users/${userId}/acitivities`, {skipLoading}).then(activities => activities.data)
 }
 
-export async function getRoleModelPosts(groupId, userId, { skipLoading } = {}) {
-    return Requests.get(`users/${userId}/postsAt/${groupId}/`, {skipLoading}).then(posts => posts.data)
+
+// BODY { searchQuery: '' }
+export async function searchActivity(groupId, post, { skipLoading } = {}) {
+    return Requests.post(`search/activities`, post, {skipLoading}).then(activities => activities.data)
 }
 
-export async function createPost(groupId, post, { skipLoading } = {}) {
-    return Requests.post(`groups/${groupId}`, post, {skipLoading}).then(post => post.data)
+// BODY { activityName: '' }
+export async function createActivity(userId, activity, { skipLoading } = {}) {
+    return Requests.post(`users/${userId}`, activity, {skipLoading}).then(activity => activity.data)
 }
 
-export async function getPost(postId, { skipLoading } = {}) {
-    return Requests.get(`posts/${postId}`, {skipLoading}).then(post => post.data)
+export async function updateActivity(activityId, activity, { skipLoading } = {}) {
+    return Requests.put(`activities/${activityId}`, activity, {skipLoading}).then(activity => activity.data)
 }
 
-export async function updatePost(postId, post, { skipLoading } = {}) {
-    return Requests.put(`posts/${postId}`, post, {skipLoading}).then(post => post.data)
+export async function deleteActivity(activityId, { skipLoading } = {}) {
+    return Requests.delete(`activities/${activityId}`, {skipLoading}).then(activity => activity.data)
 }
 
-export async function deletePost(postId, { skipLoading } = {}) {
-    return Requests.delete(`posts/${postId}`, {skipLoading}).then(post => post.data)
+export async function getAvailability(availabilityId, { skipLoading } = {}) {
+    return Requests.get(`availabilities/${availabilityId}/`, {skipLoading}).then(availabilities => availabilities.data)
+}
+
+export async function createAvailability(activityId, availability, { skipLoading } = {}) {
+    return Requests.post(`activities/${activityId}`, availability, {skipLoading}).then(availability => availability.data)
+}
+
+export async function deleteAvailability(availabilityId, availability, { skipLoading } = {}) {
+    return Requests.delete(`availabilities/${availabilityId}`, availability, {skipLoading}).then(availability => availability.data)
+}
+
+export async function getActivityAvailabilities(activityId, { skipLoading } = {}) {
+    return Requests.get(`activities/${activityId}`, {skipLoading}).then(availabilities => availabilities.data)
+}
+
+export async function updateActivityAvailability(availabilityId, availability, { skipLoading } = {}) {
+    return Requests.put(`availabilities/${availabilityId}`, availability, {skipLoading}).then(availability => availability.data)
 }
 
 export async function toggleIsMentor(userId, { skipLoading } = {}) {
